bf8b86d39d8eb2c3156695d0e6c3432b4805787c
AdS0lis <anthony.curtis@csuglobal.edu>
2024-08-04 18:03:00 -0700

    Creating the general Frame

--- CSC372 Module3 Critical Thinking Assignment/src/guiMenu/MenuGUI.java
+++ CSC372 Module3 Critical Thinking Assignment/src/guiMenu/MenuGUI.java
@@ -3,7 +3,11 @@ package guiMenu;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JFrame;
+
 public class MenuGUI implements ActionListener{
+	
+	private JFrame frame;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -13,6 +17,16 @@ public class MenuGUI implements ActionListener{
 	
 	public void GUI() {
 		
+//		Creates the frame
+		frame = new JFrame("CSC372 Module 3 CTA");
+		
+		
+		
+		// Default commands written when creating a GUI
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setVisible(true); // Make sure the frame is visible
+        frame.setLocationRelativeTo(null); // Makes the frame pop up in the middle of the screen
 	}
 
 	@Override

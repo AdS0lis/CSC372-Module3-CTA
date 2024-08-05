a7e4f8acd818936d5ff99a48afb4341b5a17fd0c
AdS0lis <anthony.curtis@csuglobal.edu>
2024-08-04 17:53:57 -0700

    Initial Creation

---
+++ CSC372 Module3 Critical Thinking Assignment/src/guiMenu/MenuGUI.java
@@ -0,0 +1,24 @@
+package guiMenu;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MenuGUI implements ActionListener{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MenuGUI simpleGui = new MenuGUI();
+		simpleGui.GUI();
+	}
+	
+	public void GUI() {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
